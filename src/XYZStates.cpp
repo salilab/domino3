750ef0587c9fc40d0de22bcdf00d3e90b8714d6d
Ben Webb <ben@salilab.org>
2014-07-01 17:14:02 -0700

    Update for latest RMF.

--- src/XYZStates.cpp
+++ src/XYZStates.cpp
@@ -25,24 +25,23 @@ void XYZStates::do_load(unsigned int i, ParticleIndex pi) const {
 }
 
 void XYZStates::add_to_rmf(ParticleIndex, RMF::NodeHandle parent) const {
-  RMF::ParticleFactory f(parent.get_file());
+  RMF::decorator::ParticleFactory f(parent.get_file());
   for (unsigned int i = 0; i < states_.size(); ++i) {
     std::ostringstream oss;
     oss << i;
     RMF::NodeHandle n = parent.add_child(oss.str(), RMF::REPRESENTATION);
-    RMF::Particle d = f.get(n);
+    RMF::decorator::Particle d = f.get(n);
     d.set_mass(1.0);
-    d.set_coordinates(RMF::Floats(states_[i].coordinates_begin(),
-                                  states_[i].coordinates_end()));
+    d.set_coordinates(RMF::Vector3(states_[i]));
   }
 }
 void XYZStates::update_rmf(ParticleIndex, RMF::NodeHandle parent,
                            Marginals *m) const {
-  RMF::ParticleFactory f(parent.get_file());
+  RMF::decorator::ParticleFactory f(parent.get_file());
   RMF::NodeHandles children = parent.get_children();
   for (unsigned int i = 0; i < states_.size(); ++i) {
     double r = max_radius_ * m->get_current_marginal(i);
-    RMF::Particle d = f.get(children[i]);
+    RMF::decorator::Particle d = f.get(children[i]);
     d.set_radius(r);
   }
 }
